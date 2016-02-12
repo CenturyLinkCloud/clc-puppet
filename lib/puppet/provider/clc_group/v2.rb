bda7febff2d73c8f16fbec120ba9c17c2ac18052
Maksim Zhylinski <maksim.zhylinski@altoros.com>
2016-02-12 16:35:24 +0300

    Wait until group deleted

--- lib/puppet/provider/clc_group/v2.rb
+++ lib/puppet/provider/clc_group/v2.rb
@@ -23,7 +23,8 @@ Puppet::Type.type(:clc_group).provide(:v2, parent: PuppetX::CenturyLink::Clc) do
   def destroy
     Puppet.info("Deleting group #{name}")
 
-    client.delete_group(group_id)
+    status = client.delete_group(group_id)
+    client.wait_for(status['id'])
 
     @property_hash[:ensure] = :absent
   end
